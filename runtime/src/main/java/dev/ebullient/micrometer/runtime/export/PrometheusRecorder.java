76f67bf1dae32fd6556dc59636834b630f984f5e
Erin Schnabel <ebullient@redhat.com>
2020-08-12 14:11:55 -0400

    Metrics endpoint produces text/plain

--- runtime/src/main/java/dev/ebullient/micrometer/runtime/export/PrometheusRecorder.java
+++ runtime/src/main/java/dev/ebullient/micrometer/runtime/export/PrometheusRecorder.java
@@ -1,7 +1,12 @@
 package dev.ebullient.micrometer.runtime.export;
 
+import java.util.function.Function;
+
 import dev.ebullient.micrometer.runtime.export.handlers.PrometheusHandler;
+import io.prometheus.client.exporter.common.TextFormat;
 import io.quarkus.runtime.annotations.Recorder;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
 
 @Recorder
 public class PrometheusRecorder {
@@ -13,5 +18,16 @@ public class PrometheusRecorder {
         }
 
         return handler;
+    }
+
+    public Function<Router, Route> route(String path) {
+        return new Function<Router, Route>() {
+            @Override
+            public Route apply(Router router) {
+                Route route = router.route(path)
+                        .produces(TextFormat.CONTENT_TYPE_004);
+                return route;
+            }
+        };
     }
 }
